4b67abeafd4fcfa0dc6e542d3838c89f05150ad0
Jonathan Wenger <jonathan.wenger@uni-tuebingen.de>
2020-09-25 10:22:40 +0200

    fixed merge conflict

--- src/probnum/_randomvariablelist.py
+++ src/probnum/_randomvariablelist.py
@@ -1,8 +1,5 @@
 import numpy as np
-<<<<<<< HEAD
-=======
 import probnum.random_variables as pnrvs
->>>>>>> 41233c8941df9990fc2e8b0e78ddab6176f65786
 
 
 class _RandomVariableList(list):
@@ -17,20 +14,6 @@ class _RandomVariableList(list):
     def __init__(self, rv_list):
         if not isinstance(rv_list, list):
             raise TypeError("RandomVariableList expects a list.")
-<<<<<<< HEAD
-        super().__init__(rv_list)
-
-    def mean(self):
-        return np.stack([rv.mean for rv in self])
-
-    def cov(self):
-        return np.stack([rv.cov for rv in self])
-
-    def var(self):
-        return np.stack([rv.var for rv in self])
-
-=======
-
         # First element as a proxy for checking all elements
         if not isinstance(rv_list[0], pnrvs.RandomVariable):
             raise TypeError("RandomVariableList expects RandomVariable elements")
@@ -49,7 +32,6 @@ class _RandomVariableList(list):
         return np.stack([rv.var for rv in self])
 
     @property
->>>>>>> 41233c8941df9990fc2e8b0e78ddab6176f65786
     def std(self):
         return np.stack([rv.std for rv in self])
 
